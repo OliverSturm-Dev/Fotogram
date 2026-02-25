a1f247aa1715aff95d38aa8e7f0811f71c2f2b35
OliverSturm-Dev <sturmoliver21@gmail.com>
2026-02-25 23:20:34 +0100

    img aadded

--- js/data.js
+++ js/data.js
@@ -14,4 +14,30 @@ const images = [
     path: "../img/snow-leopard.png",
     alt: "Schneeleopard zwischen Felsen und grünem Wald",
   },
+  {
+    title: "Meeresblick",
+    path: "../img/sea.png",
+    alt: "offenes Meer wo ein Mann auf dem Fels steht und in den nächtlichen Himmel leuchtet",
+  },
+  {
+    title: "Winterlicher See",
+    path: "../img/lake.png",
+    alt: " Ein See wo die Bäume in einer winterlichen Design sind und die Berge links voll mit Schnee sind ",
+  },
+  {
+    title: "Stadt in der Nacht",
+    path: "../img/town.png",
+    alt: "Eine Stadt in der Nacht wo viele Lichter von den Gebäuden leuchten",
+  },
+  {
+    title: "Hurricane",
+    path: "../img/hurricane.png",
+    alt: "Man sieht einen Hurricane vom Weltall aus",
+  },
+  {
+    title: "Die Atmosphäre",
+    path: "../img/atmosphere.png",
+    alt: "Das ist ein Abbild von unten von der Atmosphäre",
+  },
+  
 ];
--- js/templates.js
+++ js/templates.js
@@ -10,6 +10,6 @@ function  generateDetailContentHTML(i, image) {
     return /* html */ `
     <div class="img-test">
     <img src="${image.path}" alt="${image.alt}">
-    </button>
+    </div>
     `;
 }
