1e97ea9f7fc7aafcd2a993c8e4177c5a8d060537
OliverSturm-Dev <sturmoliver21@gmail.com>
2026-03-03 22:33:47 +0100

    key changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let currentIndex = 0;
+
 function init() {
     renderImages();
 }
@@ -15,29 +17,48 @@ function renderImages() {
 
 
 function showDetailView(i) {
+  currentIndex = i;
     const image = images[i];
     const detailBox = document.getElementById("detail_box");
 
     detailBox.classList.remove("d-none");
     detailBox.innerHTML = generateDetailContentHTML(i, image);
+    detailBox.focus();
 }
-function imageNext(i) {
-    let nextIndex = i + 1;
-  if (i >= images.length - 1) {
-    nextIndex = 0;
+function closeDetailView() {
+  const detailBox = document.getElementById("detail_box");
+
+  detailBox.classList.add("d-none");
+}
+function imageNext() {
+  currentIndex++;
+
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
   }
 
-  showDetailView(nextIndex);
+  showDetailView(currentIndex);
 }
-function imageBack(i) {
-    let nextIndex = i - 1;
-  if (i === 0) {
-    nextIndex = images.length - 1;
+
+function imageBack() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
   }
 
-  showDetailView(nextIndex);
+  showDetailView(currentIndex);
 }
-function closeBtn() {
-   const detailContainer = document.getElementById("detail_box");
-    detailContainer.classList.add("d-none");
+
+function handleOverlayKeys(event) {
+  if (event.key === "Escape") {
+    closeDetailView();
+  }
+
+  if (event.key === "ArrowRight") {
+    imageNext();
+  }
+
+  if (event.key === "ArrowLeft") {
+    imageBack();
+  }
 }
